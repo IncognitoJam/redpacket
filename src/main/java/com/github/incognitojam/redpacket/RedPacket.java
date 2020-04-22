19e49d92c9ad119feb39efb4fa7c3b026350c7f3
Cameron Clough <cameronjclough@gmail.com>
2020-04-22 16:30:22 +0100

    feat: add window demo

--- src/main/java/com/github/incognitojam/redpacket/RedPacket.java
+++ src/main/java/com/github/incognitojam/redpacket/RedPacket.java
@@ -3,19 +3,36 @@ package com.github.incognitojam.redpacket;
 import com.github.incognitojam.redpacket.engine.GameLogic;
 import com.github.incognitojam.redpacket.engine.Window;
 
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
+
 public class RedPacket implements GameLogic {
     private Window window;
+    private float color = 0.0F;
 
     @Override
     public void init() {
         // Setup window.
-        window = new Window("Red Packet", 300, 300);
-        window.setClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        window = new Window("Red Packet", 600, 300);
     }
 
     @Override
     public void update(double interval) {
-         window.update();
+        window.update();
+
+        if (window.isKeyPressed(GLFW_KEY_UP)) {
+            color += interval;
+            if (color > 1.0F)
+                color = 1.0F;
+        } else if (window.isKeyPressed(GLFW_KEY_DOWN)) {
+            color -= interval;
+            if (color < 0.0F)
+                color = 0.0F;
+        }
+
+        window.setClearColor(color, color, color, 1.0F);
+        window.setTitle(String.format("Red Packet: %f", color));
+        window.setHeight(300 + (int) (300 * color));
     }
 
     @Override
